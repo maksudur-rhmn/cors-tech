48293fd498a89b0a710337a92e6f8add017619c7
maksudur-rhmn <john.alim45@gmail.com>
2021-02-12 04:22:36 +0600

    allpush

---
+++ vendor/laravel/cashier-mollie/routes/webhooks.php
@@ -0,0 +1,17 @@
+<?php
+
+use Laravel\Cashier\Cashier;
+
+Route::namespace('\Laravel\Cashier\Http\Controllers')->group(function () {
+
+    Route::name('webhooks.mollie.default')->post(
+        Cashier::webhookUrl(),
+        'WebhookController@handleWebhook'
+    );
+
+    Route::name('webhooks.mollie.first_payment')->post(
+        Cashier::firstPaymentWebhookUrl(),
+        'FirstPaymentWebhookController@handleWebhook'
+    );
+
+});
